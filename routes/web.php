5e12adfc8274cead48fc7b463084bb2816d51ab2
Ahmad Kokab <ahmadkokab@processton.com>
2024-12-14 06:21:29 +0500

    Central Route and tenant configurations

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Central\SetupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect('login');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::prefix('tenant')->group(function () {
 
-require __DIR__.'/auth.php';
+            Route::get('/', [SetupController::class, 'store'])->name('centeral.tenant.register');
+        });
+    });
+}
