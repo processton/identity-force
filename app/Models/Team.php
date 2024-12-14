5fe7d5569f3123687b2c9a0b73780eafb1b187d3
Ahmad Kokab <ahmadkokab@processton.com>
2024-12-14 18:47:06 +0500

    Team anf user team models

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name'
+    ];
 }
--- app/Models/UserTeam.php
+++ app/Models/UserTeam.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserTeam extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'team_id'
+    ];
 }
