ed2e0f6d3021b54dac4b5cdb6ef8e4c9e0085af4
Ahmad Kokab <ahmadkokab@processton.com>
2024-12-14 07:41:03 +0500

    route file

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
-        Route::prefix('tenant')->group(function () {
 
+        Route::prefix('tenant')->group(function () {
             Route::get('/', [SetupController::class, 'store'])->name('centeral.tenant.register');
         });
     });
